9e82f2b05bfca57b8745c7e12e3377d30b344a38
giesf <64958447+giesf@users.noreply.github.com>
2023-08-04 13:37:24 +0200

    Update index.js

--- topdf/index.js
+++ topdf/index.js
@@ -14,6 +14,7 @@ class HTML5ToPDF {
       outputPath,
       launchOptions,
       pdfOptions,
+      renderDelay
     } = options
 
     const pdf = pdfOptions
@@ -22,7 +23,8 @@ class HTML5ToPDF {
     return {
       pdf,
       launchOptions,
-      inputPath
+      inputPath,
+      renderDelay
     }
   }
 
